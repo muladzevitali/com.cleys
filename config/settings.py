8f926a71769ac7ebce283e29f3824b348875995d
muladzevitali <muladzevitali@gmail.com>
2022-04-08 01:09:15 +0400

    bugfixes

--- config/settings.py
+++ config/settings.py
@@ -119,7 +119,7 @@ CKEDITOR_CONFIGS = {
 }
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = env('EMAIL_HOST')
 EMAIL_PORT = env.int('EMAIL_PORT')
 EMAIL_HOST_USER = env('EMAIL_HOST_USER')
